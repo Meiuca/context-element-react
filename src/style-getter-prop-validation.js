1065752c168519bf1902a7969915bf6054e2d14d
Levi Pires <waltvy.comercial@gmail.com>
2021-04-28 10:52:49 -0300

    style(src#style-getter-prop-validation.js): update typing

--- src/style-getter-prop-validation.js
+++ src/style-getter-prop-validation.js
@@ -1,7 +1,17 @@
-export default function styleGetter(props, propName, componentName) {
+/**
+ * Used by propTypes
+ *
+ * @type {import('prop-types').Requireable<import('@meiuca/context-element').StyleGetter>}
+ */
+const styleGetter = (props, propName, componentName) => {
   if (typeof props[propName] !== 'function' || !props[propName].extract) {
     throw new Error(
       `Invalid prop '${propName}' supplied to '${componentName}'. It is not a StyleGetter.`,
     );
   }
-}
+};
+
+// TODO: implement
+styleGetter.isRequired = null;
+
+export default styleGetter;
